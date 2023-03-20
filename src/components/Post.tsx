f150a6b8f1799a1f49ac7a133dcf2856bd19ae1c
Rafael Costa <rafaelcosta@mintysquare.com>
2023-03-20 22:53:47 +0000

    resolve issues

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -14,17 +14,22 @@ interface Author{
 }
 
 interface Content {
-    type: string;
+    type: 'paragraph' | 'link' | 'hashtag';
     content: string;
 }
 
-interface PostProps {
+export interface PostType {
+    id: number;
     author: Author;
     publishedAt: Date;
     content: Content[];
 }
 
-export function Post({ author, publishedAt, content }: PostProps) {
+interface PostProps {
+    post: PostType;
+}
+
+export function Post({ post }: PostProps) {
     const [comments, setComments] = useState([
         'Post, top'
     ])
@@ -32,11 +37,11 @@ export function Post({ author, publishedAt, content }: PostProps) {
     const [newCommentText, setNewCommentText] = useState('')
 
 
-    const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
+    const publishedDateFormatted = format(post.publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
         locale: pt,
       });
 
-      const publishedDateRelativeToNow = formatDistanceToNow( publishedAt, {
+      const publishedDateRelativeToNow = formatDistanceToNow( post.publishedAt, {
         locale: pt,
         addSuffix: true,
       })
@@ -71,10 +76,10 @@ export function Post({ author, publishedAt, content }: PostProps) {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src={author.avatarUrl}  />
+                    <Avatar src={post.author.avatarUrl}  />
                     <div className={styles.authorinfo}>
-                        <strong>{author.name} </strong>
-                        <span>{author.role} </span>
+                        <strong>{post.author.name} </strong>
+                        <span>{post.author.role} </span>
                     </div>
                 </div>
 
@@ -84,7 +89,7 @@ export function Post({ author, publishedAt, content }: PostProps) {
             </header>
 
             <div className={styles.content}>
-                {content.map(line => {
+                {post.content.map(line => {
                     if (line.type === 'paragraph') {
                         return <p key={line.content}>{line.content}</p>;
                     } else if (line.type === 'link') {
